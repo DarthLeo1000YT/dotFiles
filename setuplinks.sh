fe43008f6b30159939deb17df463915a0940e536
Randy Carranza <badrockr.email@gmail.com>
2023-03-08 23:25:35 -0500

    add installers for zsh and oh my zsh

--- setuplinks.sh
+++ setuplinks.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 cd "${0%/*}"
 echo $(pwd)
+echo "Checking if zsh is installed."
+if ! [ -x "$(command -v zsh)" ] 
+then
+    echo "zsh does't appear to be installed."
+    echo "Attempting to install via apt."
+    sudo apt install zsh
+else
+    echo "zsh appears to be already installed. Skipping..."
+fi
+
+echo "Checking if Oh My ZSH is installed"
+echo "Checking in $HOME for the .oh-my-zsh folder."
+
+if [ ! -d "$HOME/.oh-my-zsh" ]
+then
+    echo ".oh-my-zsh folder doesn't exist. Running install script."
+    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+else
+    echo ".oh-my-zsh folder exists. Assuming its installed already."
+fi
+
+echo "Symlinking repo file .zshrc to $HOME/.zshrc"
 unlink $HOME/.zshrc
 ln -s $(pwd)/.zshrc $HOME/.zshrc
+
+echo "Symlinking repo zsh theme file ubunly.zsh-theme to $HOME/.oh-my-zsh/custom/themes/ubunly.zsh-theme"
+unlink $HOME/.oh-my-zsh/custom/themes/ubunly.zsh-theme
+ln -s $(pwd)/ubunly.zsh-theme $HOME/.oh-my-zsh/custom/themes/ubunly.zsh-theme
