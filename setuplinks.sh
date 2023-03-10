f1fd1b877e8cb451165a6477f4946b5dbf2a576d
Randy Carranza <badrockr.email@gmail.com>
2023-03-09 20:03:49 -0500

    added some options

--- setuplinks.sh
+++ setuplinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "${0%/*}"
-echo $(pwd)
+#echo $(pwd)
 echo "Checking if zsh is installed."
 if ! [ -x "$(command -v zsh)" ] 
 then
@@ -11,6 +11,8 @@ else
     echo "zsh appears to be already installed. Skipping..."
 fi
 
+echo ""
+sleep 1
 echo "Checking if Oh My ZSH is installed"
 echo "Checking in $HOME for the .oh-my-zsh folder."
 
@@ -22,14 +24,45 @@ else
     echo ".oh-my-zsh folder exists. Assuming its installed already."
 fi
 
+echo ""
+sleep 1
+
+
+while true; do
+
+read -p "Do you want to set zsh as your default shell? (y/n) " yn
+
+case $yn in
+        [yY] ) echo "";
+               sudo chsh -s /bin/zsh $USER;
+               echo "You may have to reboot for this change to take effect."
+               sleep 1
+               break;;
+        [nN] ) echo "";
+               break;;
+        * ) echo "";;
+esac
+
+done
+
+echo ""
+sleep 1
+
+
 echo "Symlinking repo file .zshrc to $HOME/.zshrc"
 unlink $HOME/.zshrc
 ln -s $(pwd)/.zshrc $HOME/.zshrc
+
+echo ""
+sleep 1
 
 echo "Symlinking repo zsh theme file ubunly.zsh-theme to $HOME/.oh-my-zsh/custom/themes/ubunly.zsh-theme"
 unlink $HOME/.oh-my-zsh/custom/themes/ubunly.zsh-theme
 ln -s $(pwd)/ubunly.zsh-theme $HOME/.oh-my-zsh/custom/themes/ubunly.zsh-theme
 
+
+echo ""
+sleep 1
 
 while true; do
 
@@ -39,9 +72,24 @@ case $yn in
 	[yY] ) echo "Installing nala via apt.";
                sudo apt install nala;
 	       break;;
-	[nN] ) echo "Exiting...";
-		exit;;
+	[nN] ) echo "Exiting..."; break;;
 	* ) echo "";;
+esac
+
+done
+
+
+
+
+
+while true; do
+
+read -p "Do you want to restart the machine for changes to take effect? (y/n) " yn
+
+case $yn in
+        [yY] ) sudo reboot; break;;
+        [nN] ) echo "Exiting..."; break;;
+        * ) echo "";;
 esac
 
 done
